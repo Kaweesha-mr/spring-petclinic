a67adb6b080711310672b9e576a5a4c7455bd3a6
Kaweesha Marasinghe <110246539+Kaweesha-mr@users.noreply.github.com>
2026-01-08 07:06:04 +0530

    Update WelcomeController.java
    Signed-off-by: Kaweesha Marasinghe <110246539+Kaweesha-mr@users.noreply.github.com>

--- src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
+++ src/main/java/org/springframework/samples/petclinic/system/WelcomeController.java
@@ -5,6 +5,10 @@ package org.springframework.samples.petclinic.system;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
+
+
+
 @Controller
 class WelcomeController {
 
