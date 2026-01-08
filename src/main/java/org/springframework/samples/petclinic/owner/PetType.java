fc16f878ea920ed967e24238f61b302220126ec4
Kaweesha Marasinghe <110246539+Kaweesha-mr@users.noreply.github.com>
2026-01-08 15:11:26 +0530

    Update PetType.java
    Signed-off-by: Kaweesha Marasinghe <110246539+Kaweesha-mr@users.noreply.github.com>

--- src/main/java/org/springframework/samples/petclinic/owner/PetType.java
+++ src/main/java/org/springframework/samples/petclinic/owner/PetType.java
@@ -6,8 +6,6 @@ import org.springframework.samples.petclinic.model.NamedEntity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
-/**
-
 @Entity
 @Table(name = "types")
 public class PetType extends NamedEntity {
